8d2fd548134f949b72e98c522fcfe4ab2f43881e
Natali Schers <schersnatali@gmail.com>
2025-11-16 20:15:28 -0300

    feat: add design tokens for colors, spaces, and border radiuses

---
+++ src/shared/DesignTokens.tsx
@@ -0,0 +1,28 @@
+export const Colors = {
+    GREEN: '#009379',
+    GREEN_OPACITY: '#E5F4F2',
+    ORANGE: '#FFD883',
+    PINK: '#F2BFAF',
+    GRAY: '#F8F9FF'
+};
+
+export const Spaces = {
+    NONE: '0',
+    HALF: '4px',
+    ONE: '8px',
+    ONE_HALF: '12px',
+    TWO: '16px',
+    THREE: '24px',
+    FOUR: '32px',
+    FIVE: '40px',
+    SIX: '48px',
+    SEVEN: '56px',
+    EIGHT: '64px',
+    NINE: '72px',
+    TEN: '80px',
+};
+
+export const BorderRadiuses = {
+    NONE: '0',
+    ONE: '8px',
+};
